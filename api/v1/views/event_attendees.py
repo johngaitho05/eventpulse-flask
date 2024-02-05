c445b95680c8b067c2634a5b8bad6fe17d7f2b4d
John Gaitho <johngaitho05@gmail.com>
2024-02-05 08:40:01 +0300

    update docs

--- api/v1/views/event_attendees.py
+++ api/v1/views/event_attendees.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ objects that handle all default RestFul API actions for Event Attendees"""
+from flasgger import swag_from
 from flask import abort, jsonify, make_response
 
 from api.v1.views import app_views
@@ -9,6 +10,7 @@ from models.user import User
 
 
 @app_views.route('events/<event_id>/users', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/event_attendee/all_attendees.yml', methods=['GET'])
 def get_event_attendees(event_id):
     """
     Retrieves all event attendees
@@ -16,10 +18,11 @@ def get_event_attendees(event_id):
     event = storage.get(Event, event_id)
     if not event:
         abort(404)
-    return make_response(jsonify([user.to_dict() for user in event.attendees]), 200)
+    return make_response(jsonify([user.to_dict(anotate=['country_id']) for user in event.attendees]), 200)
 
 
 @app_views.route('events/<event_id>/users/<user_id>', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/event_attendee/post_attendee.yml', methods=['POST'])
 def post_attendee(event_id, user_id):
     """
     Add a user to the event attendees
@@ -31,10 +34,11 @@ def post_attendee(event_id, user_id):
     if user not in event.attedees:
         event.attendees.append(user)
         event.save()
-    return make_response(jsonify(user.to_dict()), 201)
+    return make_response(jsonify(user.to_dict(anotate=['country_id'])), 201)
 
 
 @app_views.route('events/<event_id>/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/event_attendee/delete_attendee.yml', methods=['DELETE'])
 def delete_attendee(event_id, user_id):
     """
     Remove a user from the event attendees
--- api/v1/views/event_tracks.py
+++ api/v1/views/event_tracks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ objects that handles all default RestFul API actions for Event Tracks"""
+from models.event import Event
 from models.event_track import EventTrack
 from models import storage
 from api.v1.views import app_views
@@ -7,36 +8,42 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
-@app_views.route('/event_tracks', methods=['GET'], strict_slashes=False)
-def get_event_tracks():
-    """
-    Retrieves all event_track objects
-    """
-    all_event_tracks = storage.all(EventTrack).values()
-    return jsonify([event_track.to_dict(anotate=['event_id']) for event_track in all_event_tracks])
-
-
 @app_views.route('events/<event_id>/event_tracks', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/event_track/all_tracks.yml', methods=['GET'])
 def get_event_tracks_by_event(event_id):
     """
     Retrieves all event_track objects
     """
     all_event_tracks = storage.filter(EventTrack, event_id=event_id).values()
-    return make_response(jsonify([event_track.to_dict() for event_track in all_event_tracks]), 200)
+    return make_response(jsonify([event_track.to_dict(anotate=['user_id']) for event_track in all_event_tracks]), 200)
 
 
-@app_views.route('/event_tracks/<event_track_id>', methods=['GET'], strict_slashes=False)
-def get_event_track(event_track_id):
-    """ Retrieves a single event_track """
-    event_track = storage.get(EventTrack, event_track_id)
-    if not event_track:
+@app_views.route('/events/<event_id>/event_tracks', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/event_track/post_track.yml', methods=['POST'])
+def post_event_track(event_id):
+    """
+    Creates a event_track
+    """
+    event = storage.get(Event, event_id)
+    if not event:
         abort(404)
+    data = request.get_json()
+    if type(data) is not dict:
+        abort(400, description="Not a JSON")
 
-    return make_response(jsonify(event_track.to_dict(anotate=['event_id'])), 200)
+    data.update({'event_id': event_id})
+    required = ['title', 'event_id', 'start_date']
+    for key in required:
+        if key not in data:
+            abort(400, description="Missing {} parameter".format(key))
+
+    instance = EventTrack(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict(anotate=['user_id'])), 201)
 
 
-@app_views.route('/event_tracks/<event_track_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/event_tracks/<event_track_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/event_track/delete_track.yml', methods=['DELETE'])
 def delete_event_track(event_track_id):
     """
     Deletes a event_track Object
@@ -53,26 +60,8 @@ def delete_event_track(event_track_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/event_tracks', methods=['POST'], strict_slashes=False)
-def post_event_track():
-    """
-    Creates a event_track
-    """
-    data = request.get_json()
-    if type(data) is not dict:
-        abort(400, description="Not a JSON")
-
-    required = ['title', 'event_id', 'start_date']
-    for key in required:
-        if key not in data:
-            abort(400, description="Missing {} parameter".format(key))
-
-    instance = EventTrack(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict(anotate=['event_id'])), 201)
-
-
 @app_views.route('/event_tracks/<event_track_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/event_track/put_track.yml', methods=['PUT'])
 def put_event_track(event_track_id):
     """
     Updates a event_track
@@ -88,4 +77,4 @@ def put_event_track(event_track_id):
         abort(400, description="Not a JSON")
 
     event_track.update(**data)
-    return make_response(jsonify(event_track.to_dict(anotate=['event_id'])), 200)
+    return make_response(jsonify(event_track.to_dict()), 200)
